4d3c0e1f18e45ea292e4ac67fdf41fe392a810b6
Rexbas <rexbasie@gmail.com>
2022-12-26 13:13:32 +0100

    Backport Pam's Harvestcraft support

--- src/main/java/com/rexbas/teletubbies/inventory/container/slot/SpecificItemSlot.java
+++ src/main/java/com/rexbas/teletubbies/inventory/container/slot/SpecificItemSlot.java
@@ -2,6 +2,9 @@ package com.rexbas.teletubbies.inventory.container.slot;
 
 import javax.annotation.Nonnull;
 
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.Tag;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.items.IItemHandler;
@@ -9,15 +12,21 @@ import net.minecraftforge.items.SlotItemHandler;
 
 public class SpecificItemSlot extends SlotItemHandler {
 
-	private final Item item;
+	public static final Tag<Item> GRAIN = ItemTags.createOptional(new ResourceLocation("forge", "grain"));
+	public static final Tag<Item> MILK = ItemTags.createOptional(new ResourceLocation("forge", "milk"));
+	public static final Tag<Item> EGG = ItemTags.createOptional(new ResourceLocation("forge", "egg"));
+	public static final Tag<Item> SUGAR = ItemTags.createOptional(new ResourceLocation("forge", "sugar"));
+	public static final Tag<Item> BOWL = ItemTags.createOptional(new ResourceLocation("teletubbies", "bowl"));
+
+	private final Tag<Item> item;
 	
-	public SpecificItemSlot(IItemHandler itemHandler, int index, int xPosition, int yPosition, Item item) {
+	public SpecificItemSlot(IItemHandler itemHandler, int index, int xPosition, int yPosition, Tag<Item> item) {
 		super(itemHandler, index, xPosition, yPosition);
 		this.item = item;
 	}
 	
     @Override
 	public boolean mayPlace(@Nonnull ItemStack stack) {
-    	return stack.getItem().equals(item);
+    	return stack.is(item);
     }
 }
